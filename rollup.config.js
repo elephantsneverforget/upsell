f4910767cfe1b86c5feae627b177f0c859d73a8c
Jonathan Cairo <88749649+elephantsneverforget@users.noreply.github.com>
2021-11-25 17:36:11 -0700

    Add <script> wrapper for compiled script.

--- rollup.config.js
+++ rollup.config.js
@@ -7,11 +7,28 @@ const config = {
         file: 'dist/main.js',
         format: 'esm',
     },
-    plugins: [babel({ babelHelpers: 'bundled' }), terser({
-        format: {
-            preamble: "/* Elevar OCU Upsell Script */",
+    plugins: [
+        babel({ babelHelpers: 'bundled' }),
+        terser(),
+        {
+            name: 'wrapper',
+            generateBundle(outputOptions, bundle) {
+                console.log(Object.keys(bundle));
+                const entry = Object.values(bundle).find((chunk) => { 
+                    console.log(chunk);
+                    return chunk.isEntry
+                });
+                // Deleting this prevents the standard main.js file output. If you want to go back to the standard output delete the line below and remove this.emit file.                
+                delete bundle['main.js'];
+                // Emits extra file. You can prevent files from being emitted if you delete them from the bundle object.
+                this.emitFile({
+                    type: 'asset',
+                    fileName: 'main.js',
+                    source: '<script>' + entry.code + '</script>'
+                });
+            }
         }
-    })]
+    ],
 };
 
 export default config;
