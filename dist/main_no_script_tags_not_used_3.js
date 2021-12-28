0d1937adfdbfb8c90abb198aaa6921f4f0139cab
Jonathan Cairo <88749649+elephantsneverforget@users.noreply.github.com>
2021-12-27 20:31:22 -0700

    Cache bust

--- dist/main_no_script_tags_not_used_3.js
+++ dist/main_no_script_tags_not_used_3.js
@@ -381,15 +381,11 @@ try {
 var upsellCount = 0;
 
 (function (w, d, s, l, i) {
-  w[l] = w[l] || [];
-  w[l].push({
-    "gtm.start": new Date().getTime(),
-    event: "gtm.js"
-  });
-  var f = d.getElementsByTagName(s)[0],
-      j = d.createElement(s),
-      dl = l != "dataLayer" ? "&l=" + l : "";
-  j.async = true;
-  j.src = "https://ssapi.everiewoman.com/gtm.js?id=" + i + dl;
-  f.parentNode.insertBefore(j, f);
-})(window, document, "script", "dataLayer", "GTM-MDQNKTX");
+  w[l] = w[l] || []; w[l].push({
+      'gtm.start':
+          new Date().getTime(), event: 'gtm.js'
+  }); var f = d.getElementsByTagName(s)[0],
+      j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+          'https://sstagging.beautybyearth.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+})(window, document, 'script', 'dataLayer', 'GTM-KQ8R2LQ');
+})();
